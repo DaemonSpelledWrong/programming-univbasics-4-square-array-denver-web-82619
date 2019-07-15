2a67227cec8c24a879e9d91922aee0a7e8c4671b
Damon Self <damongself@gmail.com>
2019-07-15 10:54:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   counter = 0
+  newArray = []
   
   while counter < array.length do
-    array[counter] ** 2
+    newArray.push(array[counter] ** 2)
     counter += 1
   end
 end
